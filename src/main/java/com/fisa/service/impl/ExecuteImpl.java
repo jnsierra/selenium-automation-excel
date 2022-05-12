c645bb0c0fd15ee648a17e36b90ea09aac559328
jnsierra <jnsierrac@gmail.com>
2022-05-12 07:53:54 -0500

    Ajuste para que al generar una excepcion se cierre el browser

--- src/main/java/com/fisa/service/impl/ExecuteImpl.java
+++ src/main/java/com/fisa/service/impl/ExecuteImpl.java
@@ -3,6 +3,7 @@ package com.fisa.service.impl;
 import com.fisa.service.AutomationExecute;
 import com.fisa.service.Execute;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.WebDriver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
@@ -13,9 +14,12 @@ public class ExecuteImpl implements Execute {
     private static final Logger logger = Logger.getLogger(ExecuteImpl.class);
     private AutomationExecute automationExecute;
 
+    private WebDriver driver;
+
     @Autowired
-    public ExecuteImpl(AutomationExecute automationExecute) {
+    public ExecuteImpl(AutomationExecute automationExecute,WebDriver driver) {
         this.automationExecute = automationExecute;
+        this.driver = driver;
     }
 
     @EventListener(ApplicationReadyEvent.class)
@@ -25,7 +29,9 @@ public class ExecuteImpl implements Execute {
         try {
             this.automationExecute.executeAutomation();
         } catch (InterruptedException e) {
+            logger.error("Se cierra el browser al generar un error");
             logger.error(e);
+            this.driver.quit();
             throw new RuntimeException(e);
         }
     }
